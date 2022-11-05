bf0f6bae7dfdc6cadb6093126e4a71ff0912ae56
Yasushi Takehara <yasushitakehara@Yasushis-MacBook-Air-2020.local>
2022-11-06 04:41:54 +0900

    update

--- test/util/date_format_util_test.dart
+++ test/util/date_format_util_test.dart
@@ -1,23 +1,11 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:dacapo/util/date_format_util.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dacapo/main.dart';
-import 'package:intl/intl.dart';
-
 // expect yyyyMMddTHHmmss
-final _scoreDtoIdFormat = new RegExp(r'^\d{8}T\d{6}$');
+final _scoreDtoIdFormat = RegExp(r'^\d{8}T\d{6}$');
 
 void main() {
   test('createScoreDtoID', () {
-    // Build our app and trigger a frame.
     String actualStr = DateFormatUtil.createScoreDtoID();
     expect(_scoreDtoIdFormat.hasMatch(actualStr), isTrue);
 
@@ -25,6 +13,6 @@ void main() {
     final diffMS = DateTime.now().difference(actualDateTime).inMilliseconds;
 
     // actualDateTime must be near current date time.
-    expect(diffMS, lessThanOrEqualTo(1000));
+    expect(diffMS, lessThanOrEqualTo(2000));
   });
 }
